66f57efed126b600854e0593376f4767a8fbf2a1
Jackson Fitch <fitchj23@students.ecu.edu>
2024-04-14 21:57:10 -0400

    Update

--- class_activity/1000Array.cpp
+++ class_activity/1000Array.cpp
@@ -2,41 +2,38 @@
 #include <cstdlib>
 #include <ctime>
 
+using namespace std;
+
 void displayArray(int arr[], int size) {
     for (int i = 0; i < size; i++) {
-        std::cout << arr[i] << " ";
+        cout << arr[i] << " ";
     }
-    std::cout << std::endl;
+    cout << endl;
 }
 
 int main() {
     const int SIZE = 1000;
     int arr[SIZE];
 
-    // Initialize the first 10 elements with random numbers between [0, 100]
     srand(time(0));
     for (int i = 0; i < 10; i++) {
         arr[i] = rand() % 101;
     }
 
-    // Display the current elements
     displayArray(arr, 10);
 
-    // Append 80 to the end of the list
     arr[10] = 80;
 
-    // Insert 50 as the new second element
     for (int i = 11; i > 1; i--) {
         arr[i] = arr[i - 1];
     }
+
     arr[1] = 50;
 
-    // Delete the third element from the list
     for (int i = 2; i < 11; i++) {
         arr[i] = arr[i + 1];
     }
 
-    // Display the updated elements
     displayArray(arr, 11);
 
     return 0;
