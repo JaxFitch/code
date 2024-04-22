f3fb6608ddee75ef7c29af6a844aed520af57bae
Jackson Fitch <fitchj23@students.ecu.edu>
2024-04-21 22:19:53 -0400

    Added palindrome homework

---
+++ csci-1010/class_activity/palindrome.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+using namespace std;
+
+bool isPalindrome(const string str) {
+    int left = 0;
+    int right = str.length() - 1;
+
+    while(left < right) {
+        if (str[left] != str[right]) {
+            return false;
+        }
+        left++;
+        right--;
+    }
+
+    return true;
+}
+
+int main() {
+    string input;
+    
+    cout << "Enter a string: ";
+    cin >> input;
+
+    if (isPalindrome(input)) {
+        cout << "The string is a palindrome." << endl;
+    } else {
+        cout << "The string is not a palindrome." << endl;
+    }
+
+    return 0;
+}
